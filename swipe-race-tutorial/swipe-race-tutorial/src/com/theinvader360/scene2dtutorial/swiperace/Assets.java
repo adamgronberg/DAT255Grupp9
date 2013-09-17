17b5e11eb384c443211d36e53a61570ca326828c
Magnus Dannerstedt <magnus.dannerstedt@gmail.com>
2013-09-17 18:50:09 +0200

    exempel med space cars

---
+++ swipe-race-tutorial/swipe-race-tutorial/src/com/theinvader360/scene2dtutorial/swiperace/Assets.java
@@ -0,0 +1,23 @@
+package com.theinvader360.scene2dtutorial.swiperace;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Assets {
+	public static TextureAtlas atlas;
+	public static TextureRegion car;
+	public static TextureRegion road;
+
+	public static void load() {
+		atlas = new TextureAtlas(Gdx.files.internal("images.atlas"));
+		car = atlas.findRegion("car");
+		road=new TextureRegion ( new Texture(Gdx.files.internal("space-1.png")));
+		//road = atlas.findRegion("road");
+	}
+
+	public static void dispose() {
+		atlas.dispose();
+	}
+}
