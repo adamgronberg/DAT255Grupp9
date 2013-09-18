1608fa9fc93399c9f260efef0a6224470dafcd60
adamgronberg <adam.gronberg@outlook.com>
2013-09-18 11:38:39 +0200

    Added left and right controls. (Now invisible, work in progress)

--- swipe-race-tutorial/swipe-race-tutorial/src/com/theinvader360/scene2dtutorial/swiperace/GameScreen.java
+++ swipe-race-tutorial/swipe-race-tutorial/src/com/theinvader360/scene2dtutorial/swiperace/GameScreen.java
@@ -6,15 +6,26 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.input.GestureDetector.GestureListener;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class GameScreen implements Screen, GestureListener {
 	private Stage stage;
+	private Button button;
+	private ButtonStyle buttonStyle;
 	private TrafficGame trafficGame;
 
+	public static final String LOG = GameScreen.class.getSimpleName();
+	
+	
 	public GameScreen() {
 		stage = new Stage();
 		trafficGame = new TrafficGame();
+		//buttonStyle.checked.draw(batch, 0, 0, 15,15);
+//			button = new Button();
 		stage.addActor(trafficGame);
 	}
 	
@@ -29,6 +40,22 @@ public class GameScreen implements Screen, GestureListener {
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		stage.act(delta);
 		stage.draw();
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			
+			//bucket.x = touchPos.x - 64 / 2;
+			if (touchPos.x <= 90 && touchPos.y >= 710) {
+				Gdx.app.log( GameScreen.LOG, "Pressed x :" + touchPos.x + " y : " + touchPos.y  );
+				trafficGame.playerCar.tryMoveLeft();
+			}
+			if (touchPos.x >= 400 && touchPos.y >= 710) {
+				Gdx.app.log( GameScreen.LOG, "Pressed x :" + touchPos.x + " y : " + touchPos.y  );
+				trafficGame.playerCar.tryMoveRight();
+			}
+		}
+		
+		
 	}
 
 	@Override
@@ -52,7 +79,11 @@ public class GameScreen implements Screen, GestureListener {
 	@Override public void pause() {}
 	@Override public void dispose() {}	
 	@Override public boolean tap(float x, float y, int count, int button) {return false;}
-	@Override public boolean touchDown(float x, float y, int pointer, int button) {return false;}
+	@Override public boolean touchDown(float x, float y, int pointer, int button) {
+		//Gdx.app.log( GameScreen.LOG, "Pressed x :" + x + " y : " + y  );
+		
+		return false;
+	}
 	@Override public boolean longPress(float x, float y) {return false;}
 	@Override public boolean pan(float x, float y, float deltaX, float deltaY) {return false;}
 	@Override public boolean zoom(float initialDistance, float distance) {return false;}
