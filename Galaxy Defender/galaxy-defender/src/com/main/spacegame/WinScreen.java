b8f0046502689c2554a54772f5abe67f6257f982
chipsetHD <jl_inbox@hotmail.com>
2013-10-10 15:02:20 +0200

    Added buttons to winscreen
    empty buttons for use in weapons upgrades

--- Galaxy Defender/galaxy-defender/src/com/main/spacegame/WinScreen.java
+++ Galaxy Defender/galaxy-defender/src/com/main/spacegame/WinScreen.java
@@ -20,11 +20,11 @@ public class WinScreen implements Screen,InputProcessor{
 	private String text="Enter Your Name: ";
 	private Stage stage;
 	private Skin skin;
-	private TextButton continueButton;
+	private TextButton continueButton,laserButton,missileButton,empButton;
 	private Label youWinLabel;
 	private Label highScore;
 	private MyGame myGame;
-	private Table table;
+	private Table table,table2;
 	private TextureAtlas atlas;
 	private TextureRegionDrawable menuBackground;
 	private int level;
@@ -34,8 +34,7 @@ public class WinScreen implements Screen,InputProcessor{
 	
 	public WinScreen(MyGame myGame, int level){
 		this.myGame = myGame;
-		this.level = level;
-		
+		this.level = level;	
 	}
 	
 	/**
@@ -46,8 +45,7 @@ public class WinScreen implements Screen,InputProcessor{
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);	
 		stage.act();
-		stage.draw();
-		
+		stage.draw();	
 	}
 
 	/**
@@ -65,25 +63,24 @@ public class WinScreen implements Screen,InputProcessor{
 	 * Called when app is resumed on android
 	 */
 	@Override public void show() {
-		
-		
+			
 		stage = new Stage();
 		atlas = new TextureAtlas("uiskin.atlas");
 		menuBackground = new TextureRegionDrawable(assets.ImageAssets.mainMenu);
 		
 		skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
 		table = new Table(skin);
-		table.setBounds(0, 0,MyGame.WIDTH,MyGame.HEIGHT);
+		table2 = new Table(skin);
+		table.setBounds(0, 0,MyGame.WIDTH,MyGame.HEIGHT);		
 		table.setBackground(menuBackground);
+		table2.setBounds(0, 0, MyGame.WIDTH, MyGame.HEIGHT/2);
 		
 		highScore=new Label(text,skin);
-		
 		youWinLabel = new Label(labelTexts[level],skin);
 		table.add(youWinLabel).spaceBottom(50).row();
 		
 		if(level==0){
-			Gdx.input.setOnscreenKeyboardVisible(true);
-			
+			Gdx.input.setOnscreenKeyboardVisible(true);	
 			table.add(highScore).spaceBottom(100).row();
 			highScore.setText(text);
 		}
@@ -96,9 +93,36 @@ public class WinScreen implements Screen,InputProcessor{
 	        }
 	    } );
 		
-		continueButton.pad(15, 10, 15, 10);		
-		table.add(continueButton).spaceBottom(50).row();		
+		laserButton = new TextButton("Laser",skin);
+		laserButton.addListener(new ClickListener() {	       
+	        public void clicked(InputEvent event,float x,float y )
+	        {
+	        	;
+	        }
+	    } );
+		
+		missileButton = new TextButton("Missile",skin);
+		missileButton.addListener(new ClickListener() {	       
+	        public void clicked(InputEvent event,float x,float y )
+	        {
+	        	;
+	        }
+	    } );
+		
+		empButton = new TextButton("EMP",skin);
+		empButton.addListener(new ClickListener() {	       
+	        public void clicked(InputEvent event,float x,float y )
+	        {
+	        	;
+	        }
+	    } );
+			
+		table.add(continueButton).width(100).height(50);
+		table2.add(laserButton).width(100).height(50).spaceRight(30);
+		table2.add(missileButton).width(100).height(50).spaceRight(30);
+		table2.add(empButton).width(100).height(50);
 		stage.addActor(table);
+		stage.addActor(table2);
 				
 		InputMultiplexer multiplexer = new InputMultiplexer();
 		multiplexer.addProcessor(stage);
@@ -139,8 +163,7 @@ public class WinScreen implements Screen,InputProcessor{
 	 * Saves the current score from the game
 	 * @param score
 	 */
-	
-	public void setScore(int score){
+		public void setScore(int score){
 		this.score=score;
 	}
 	
@@ -150,45 +173,12 @@ public class WinScreen implements Screen,InputProcessor{
 	
 	
 	////////////// Unused methods  /////////////////
-	@Override
-	public boolean keyDown(int keycode) {return false;}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	@Override	public boolean keyDown(int keycode) {return false;}
+	@Override	public boolean keyUp(int keycode) {return false;}
+	@Override	public boolean touchDown(int screenX, int screenY, int pointer, int button) {return false;}
+	@Override	public boolean touchUp(int screenX, int screenY, int pointer, int button) {	return false;}
+	@Override	public boolean touchDragged(int screenX, int screenY, int pointer) {return false;}
+	@Override	public boolean mouseMoved(int screenX, int screenY) {return false;}
+	@Override	public boolean scrolled(int amount) {return false;}
 
 }
