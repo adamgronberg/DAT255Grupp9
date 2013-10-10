4ce3f4e23fd37c02bb61da110d08dc31521cc5e0
nikhel <niklas_0321@hotmail.com>
2013-10-10 13:37:15 +0200

    highscore added sorting

--- Galaxy Defender/galaxy-defender/src/com/main/highscore/HighscoreHandler.java
+++ Galaxy Defender/galaxy-defender/src/com/main/highscore/HighscoreHandler.java
@@ -9,11 +9,17 @@ public class HighscoreHandler {
 	private Array<User> highscore;
 	private User currentUser;
 	
-	 
+	/**
+	 * constructor
+	 */	 
     private HighscoreHandler() {  
     	highscore = new Array<User>();
     }
-
+    
+    /**
+     * 
+     * @return the instance of HighscoreHandler
+     */
     public static HighscoreHandler getInstance() {
             if (instance == null) {
                         if (instance == null) {
@@ -23,16 +29,43 @@ public class HighscoreHandler {
             return instance;
     }
     
-    public void createPlayer(String name){
-    	currentUser = new User(0,name);
+    /**
+     * 
+     * @param name The name of the new player
+     * 
+     */
+    public void createPlayer(String name, int score){
+    	currentUser = new User(score,name);
     }
     
+    /**
+     * adding the current user to highscore list if the point is good enough
+     */
     protected void addPlayerToHighscore(){
-    	for(int i = 0; i<NUMBEROFHEROES; i++){
-    		if(highscore.get(i).getScore()<currentUser.getScore()){
-    			
-    		}
+    	if(highscore.size<NUMBEROFHEROES){
+    		highscore.add(currentUser); 
+    		return;
+    	}
+    	
+    	if(currentUser.getScore()>highscore.get(highscore.size).getScore()){
+    		highscore.removeIndex(highscore.size);
+    		highscore.add(currentUser);   
     	}
     }
+    
+    /**
+     * Fix the highscore so that they are sorted by points
+     */
+    protected void sortHighscore(){
+    	for(int i = 0; i<highscore.size; i++){
+    		for(int j = i; j<highscore.size; j++){
+        		if(highscore.get(i).getScore()<highscore.get(j).getScore()){
+        			highscore.swap(i, j);
+        		}
+        	}
+    	}
+    }
+    
+    
 
 }
